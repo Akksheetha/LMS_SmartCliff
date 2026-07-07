8e0ce6e730580f8a70a8a60a907f4d0deb90cad7
Vithyamurugesan <vithyabtech54@gmail.com>
2026-07-07 14:29:11 +0530

    Implemented the CustomWorld

---
+++ src/test/World/CustomWorld.ts
@@ -0,0 +1,7 @@
+import {Browser,BrowserContext,Page} from "@playwright/test";
+export class CustomWorld{
+    browser!:Browser;
+    context!:BrowserContext;
+    page!:Page;
+    //logger=logger;
+}
