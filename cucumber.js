dee52c720dd90cf37b24c0b44661e3b7f3122b08
jeevapranesh005 <jeevapranesh.vrp@gmail.com>
2026-07-07 14:27:06 +0530

    Added cucumber.js File

---
+++ cucumber.js
@@ -0,0 +1,37 @@
+module.exports={
+    default:{
+        "formatOptions":{
+            "snippetInterface":"async-await"
+        },
+        requireModule:[
+            "ts-node/register"
+        ],
+
+        
+
+        require:[
+            "src/test/StepDefinitions/**/*.ts",
+            "src/test/Hooks/**/*.ts",
+            "src/test/support/**/*.ts",
+           
+        ],
+
+        
+
+        paths:[
+            "src/test/Features/**/*.feature",
+        ],
+
+        publishQuiet:true,
+        dryRun:false,  //false for real automation
+
+        format: [
+            "progress",
+            "progress-bar",
+            "html:Report/cucumber-html/cucumber-report.html",
+            "json:Report/cucumber-json/report.json",
+            "rerun:rerun/rerun.txt"
+]
+
+    }
+};
