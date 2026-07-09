51036865bc59b3e15dab2d396a2f4cbb8a7f0c07
Vithyamurugesan <vithyabtech54@gmail.com>
2026-07-09 12:21:35 +0530

    Filter feature updated

--- src/test/Pages/FilterPage.ts
+++ src/test/Pages/FilterPage.ts
@@ -1,5 +1,6 @@
 import { expect, Locator, Page } from "@playwright/test";
 import { basepage } from "./basePage";
+
 export class FilterPage extends basepage {
 
     readonly courseManagement: Locator;
@@ -10,53 +11,28 @@ export class FilterPage extends basepage {
     constructor(page: Page) {
         super(page);
 
-        // Left side Course Management icon/menu
-        this.courseManagement = page.locator('.p-1\\.5.bg-blue-100');
-        
-         
-
-        // Filters button
+        this.courseManagement = page.locator('[title="Course Management"]');
         this.filtersButton = page.getByRole('button', { name: 'Filters' });
-
-        // Category dropdown
-        this.categoryDropdown = page.getByRole('combobox').nth(1)
-        // OR use this if the above doesn't work:
-        // this.categoryDropdown = page.locator("//select[option[text()='All Categories']]");
-
-        // Category column in the table
+        this.categoryDropdown = page.getByRole('combobox').nth(1);
         this.courseCategory = page.locator("//tbody/tr/td[4]");
     }
 
     async clickCourseManagement() {
-
         await this.click(this.courseManagement);
-
     }
 
     async clickFilters() {
-
         await this.click(this.filtersButton);
-
     }
 
     async selectCategory(category: string) {
-
-        await this.categoryDropdown.selectOption({
-            label: category
-        });
-
+    await this.categoryDropdown.selectOption({ value: category });
     }
 
     async verifySelectedCategory(category: string) {
-
         const count = await this.courseCategory.count();
-
         for (let i = 0; i < count; i++) {
-
             await expect(this.courseCategory.nth(i)).toContainText(category);
-
         }
-
     }
-
 }
