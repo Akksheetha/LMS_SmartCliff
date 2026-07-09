c714fa722f02fba8c01f8eba8d10f0a11acbd236
Jananisri14 <jananisathishkumar380@gmail.com>
2026-07-09 12:35:03 +0530

    Timeout Added

--- src/test/Pages/SearchPage.ts
+++ src/test/Pages/SearchPage.ts
@@ -47,11 +47,11 @@ export class searchPage extends basepage {
     }
 
     async assertCourseDisplayed(expectedCourse: string) {
-        await this.page.waitForTimeout(10000);
+        await this.page.waitForTimeout(30000);
         await expect(this.courseNames).toContainText(expectedCourse);
     }
 async assertNoRecords() {
-    await this.page.waitForTimeout(10000);
+    await this.page.waitForTimeout(30000);
 
     await expect(this.noUsersMessage).toBeVisible();
 
